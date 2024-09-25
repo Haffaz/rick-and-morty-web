998dcafa36a733578b0e1fa5f0d38beb802a531e
haffaz <mohomed.haffaz@anyfin.com>
2024-09-26 01:06:20 +0200

    overide breakpoints

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
+    screens: {
+      sm: "640px",
+      // => @media (min-width: 640px) { ... }
+      md: "700px",
+      // => @media (min-width: 700px) { ... }
+      lg: "1024px",
+      // => @media (min-width: 1024px) { ... }
+      xl: "1280px",
+      // => @media (min-width: 1280px) { ... }
+    },
   },
   plugins: [],
-}
-
+};
